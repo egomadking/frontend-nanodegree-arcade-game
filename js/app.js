5a0759db010725ec64ae2f7da72cdd5b1266de72
Roy Mosby <ego.madking@gmail.com>
2017-10-13 14:56:03 -0400

    instantiated player

--- js/app.js
+++ js/app.js
@@ -53,9 +53,10 @@ Player.prototype.handleInput = function(/*what param?*/) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = 
+var allEnemies = [];
 
 
+var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -66,5 +67,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    Player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
